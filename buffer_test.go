8abd48f6cc08cc104605cad41b95799a4939eea4
haiswork <haiswork@hotmail.com>
2019-03-23 08:44:55 +0800

    update test case

--- buffer_test.go
+++ buffer_test.go
@@ -2,179 +2,114 @@ package hbuffer
 
 import (
 	"testing"
-
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestBuffer(t *testing.T) {
-	Convey("Check Buffer Funcs.", t, func() {
-		b := NewBuffer()
-		Convey("Check Write & Read Bool.", func() {
-			b.WriteBool(true)
-			b.WriteBool(false)
-			b.SetPosition(0)
-			boo1, _ := b.ReadBool()
-			So(boo1, ShouldBeTrue)
-			boo2, _ := b.ReadBool()
-			So(boo2, ShouldBeFalse)
-			So(b.GetPosition(), ShouldEqual, 2)
-			So(b.Len(), ShouldEqual, 2)
-		})
-		Convey("Check Write & Read Int32.", func() {
-			b.WriteInt32(133)
-			b.SetPosition(0)
-			i, _ := b.ReadInt32()
-			So(i, ShouldEqual, 133)
-			So(b.GetPosition(), ShouldEqual, 2) // varint len
-			So(b.Len(), ShouldEqual, 2)
-		})
-		Convey("Check Write & Read Uint32.", func() {
-			b.WriteUint32(133)
-			b.SetPosition(0)
-			i, _ := b.ReadUint32()
-			So(i, ShouldEqual, 133)
-			So(b.GetPosition(), ShouldEqual, 2)
-			So(b.Len(), ShouldEqual, 2)
-		})
-		Convey("Check Write & Read Uint64.", func() {
-			b.WriteUint64(133)
-			b.SetPosition(0)
-			i, _ := b.ReadUint64()
-			So(i, ShouldEqual, 133)
-			So(b.GetPosition(), ShouldEqual, 2)
-			So(b.Len(), ShouldEqual, 2)
-		})
-		Convey("Check Write & Read Int64.", func() {
-			b.WriteInt64(133)
-			b.SetPosition(0)
-			i, _ := b.ReadInt64()
-			So(i, ShouldEqual, 133)
-			So(b.GetPosition(), ShouldEqual, 2)
-			So(b.Len(), ShouldEqual, 2)
-		})
-		Convey("Check Write & Read Float32.", func() {
-			b.WriteFloat32(133.33)
-			b.SetPosition(0)
-			i, _ := b.ReadFloat32()
-			So(i, ShouldEqual, 133.33)
-			So(b.GetPosition(), ShouldEqual, 4)
-			So(b.Len(), ShouldEqual, 4)
-		})
-		Convey("Check Write & Read Float64.", func() {
-			b.WriteFloat64(133.33)
-			b.SetPosition(0)
-			i, _ := b.ReadFloat64()
-			So(i, ShouldEqual, 133.33)
-			So(b.GetPosition(), ShouldEqual, 8)
-			So(b.Len(), ShouldEqual, 8)
-		})
-		Convey("Check Write & Read String.", func() {
-			src := "test_abc一二三"
-			b.WriteString(src)
-			b.SetPosition(0)
-			s, _ := b.ReadString()
-			So(s, ShouldEqual, src)
-			So(b.GetPosition(), ShouldEqual, len(src)+1)
-			So(b.Len(), ShouldEqual, len(src)+1)
-		})
-		Convey("Check Write & Get Bytes.", func() {
-			bytes := []byte{1, 2, 3, 4, 5}
-			b.WriteBytes(bytes)
-			bs := b.GetBytes()
-			So(len(bs), ShouldEqual, len(bytes))
-			for i := range bs {
-				So(bs[i], ShouldEqual, bytes[i])
-			}
-		})
-		Convey("Check Write & Read All Types.", func() {
-			b.WriteBool(true)
-			b.WriteInt32(123)
-			b.WriteInt64(124)
-			b.WriteUint32(125)
-			b.WriteUint64(126)
-			b.WriteFloat32(122.33)
-			b.WriteFloat64(122.44)
-			b.WriteString("test_abc")
-			b.SetPosition(0)
+	b := NewBuffer()
+	// read&write bool
+	b.WriteBool(true)
+	b.WriteBool(false)
+	b.SetPosition(0)
+	boo1, _ := b.ReadBool()
+	shouldBeBool(boo1, true, t)
+	boo2, _ := b.ReadBool()
+	shouldBeBool(boo2, false, t)
+	shouldEqual(b.GetPosition(), 2, t)
+	shouldEqual(b.Len(), 2, t)
 
-			boo, _ := b.ReadBool()
+	// read&write int32
+	b.Reset()
+	b.WriteInt32(133)
+	b.SetPosition(0)
+	i32, _ := b.ReadInt32()
+	shouldEqual(i32, int32(133), t)
 
-			So(boo, ShouldBeTrue)
+	// read&write uint32
+	b.WriteUint64(133)
+	b.SetPosition(0)
+	u32, _ := b.ReadUint32()
+	shouldEqual(u32, uint32(133), t)
 
-			i32, _ := b.ReadInt32()
+	// read&write int64
+	b.Reset()
+	b.WriteInt64(133)
+	b.SetPosition(0)
+	i64, _ := b.ReadInt64()
+	shouldEqual(i64, int64(133), t)
 
-			So(i32, ShouldEqual, 123)
+	// read&write uint64
+	b.Reset()
+	b.WriteUint64(133)
+	b.SetPosition(0)
+	u64, _ := b.ReadUint32()
+	shouldEqual(u64, uint32(133), t)
 
-			i64, _ := b.ReadInt64()
+	// read&write float32
+	b.Reset()
+	b.WriteFloat32(133.33)
+	b.SetPosition(0)
+	f32, _ := b.ReadFloat32()
+	shouldEqual(f32, float32(133.33), t)
+	shouldEqual(b.GetPosition(), 4, t)
+	shouldEqual(b.Len(), 4, t)
 
-			So(i64, ShouldEqual, 124)
+	// read&write float64
+	b.Reset()
+	b.WriteFloat64(133.33)
+	b.SetPosition(0)
+	f64, _ := b.ReadFloat64()
+	shouldEqual(f64, float64(133.33), t)
+	shouldEqual(b.GetPosition(), 8, t)
+	shouldEqual(b.Len(), 8, t)
 
-			ui32, _ := b.ReadUint32()
+	// read&write string
+	b.Reset()
+	src := "test_abc一二三"
+	b.WriteString(src)
+	b.SetPosition(0)
+	s, _ := b.ReadString()
+	shouldEqual(src, s, t)
 
-			So(ui32, ShouldEqual, 125)
+	// io.reader & io.writer
+	b.Reset()
+	bytes := []byte{0, 1, 2, 3}
+	n, e := b.Write(bytes)
+	shouldEqual(n, len(bytes), t)
+	shouldEqual(e, nil, t)
 
-			ui64, _ := b.ReadUint64()
+	b.SetPosition(0)
+	newBytes := make([]byte, 2)
+	n, e = b.Read(newBytes)
+	shouldEqual(n, len(newBytes), t)
+	shouldEqual(e, nil, t)
 
-			So(ui64, ShouldEqual, 126)
-
-			f32, _ := b.ReadFloat32()
-
-			So(f32, ShouldEqual, 122.33)
-
-			f64, _ := b.ReadFloat64()
-
-			So(f64, ShouldEqual, 122.44)
-
-			s, _ := b.ReadString()
-
-			So(s, ShouldEqual, "test_abc")
-		})
-
-		Convey("Check io.Writer & io.Reader", func() {
-			bytes := []byte{0, 1, 2, 3}
-			n, e := b.Write(bytes)
-			So(n, ShouldEqual, 4)
-			So(e, ShouldBeNil)
-
-			b.SetPosition(0)
-			newBytes := make([]byte, 2)
-			n, e = b.Read(newBytes)
-			So(n, ShouldEqual, len(newBytes))
-			So(e, ShouldBeNil)
-
-			b.SetPosition(0)
-			newBytes = make([]byte, 10)
-			n, e = b.Read(newBytes)
-			So(n, ShouldEqual, b.Len())
-			So(e, ShouldBeNil)
-		})
-	})
+	b.SetPosition(0)
+	newBytes = make([]byte, 10)
+	n, e = b.Read(newBytes)
+	shouldEqual(n, len(bytes), t)
+	shouldEqual(e, nil, t)
 }
 
 func TestBufferErrs(t *testing.T) {
-	Convey("Check Buffer Funcs.", t, func() {
-		b := NewBuffer()
-		Convey("Check Read Bytes Err.", func() {
-			_, e := b.ReadBytes(1 << 10)
-			So(e, ShouldNotBeNil)
-		})
-	})
+	b := NewBuffer()
+	_, e := b.ReadBytes(1 << 10)
+	shouldNotEqual(e, nil, t)
 }
 
-func TestBufferDeleteBefore(t *testing.T) {
-	Convey("Check Buffer Funcs.", t, func() {
-		b := NewBuffer()
-		Convey("Check DeleteBefor.", func() {
-			b.WriteBytes([]byte{1, 2, 3})
-			b.DeleteBefore(2)
-			So(b.Len(), ShouldEqual, 1)
-			bt, _ := b.ReadByte()
-			So(bt, ShouldEqual, 3)
-		})
-		Convey("Check DeleteBefore All.", func() {
-			b.WriteBytes([]byte{1, 2, 3})
-			b.DeleteBefore(5)
-			So(b.Len(), ShouldEqual, 0)
-		})
-	})
+func shouldBeBool(b, equal bool, t *testing.T) {
+	if b != equal {
+		t.Fatal("not be bool")
+	}
+}
+
+func shouldEqual(a, b interface{}, t *testing.T) {
+	if a != b {
+		t.Fatalf("%v != %v", a, b)
+	}
+}
+
+func shouldNotEqual(a, b interface{}, t *testing.T) {
+	if a == b {
+		t.Fatalf("%v == %v", a, b)
+	}
 }
